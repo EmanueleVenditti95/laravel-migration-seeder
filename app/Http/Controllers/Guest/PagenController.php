98952620c78f122417a598b0070947ad35d9b5dc
EmanueleVenditti95 <vendittiema@gmail.com>
2023-12-13 17:36:42 +0100

    Add - trains filtered by date

--- app/Http/Controllers/Guest/PagenController.php
+++ app/Http/Controllers/Guest/PagenController.php
@@ -8,10 +8,21 @@ use App\Models\Train;
 
 class PagenController extends Controller
 {
-    public function index() {
-        $title = 'Treni';
-        $trains = Train::all();
+    // public function index() {
+    //     $title = 'Treni';
+    //     $trains = Train::all();
 
-        return view('index',compact('trains','title'));
+    //     return view('index',compact('trains','title'));
+    // }
+
+    public function index(){
+        $title = 'Treni di oggi';
+
+        $builder = Train::where('departure_time','like','2023-12-13%');
+        // dd($builder);
+
+        $trains = $builder->get();
+
+        return view('index',compact('title','trains'));
     }
 }
