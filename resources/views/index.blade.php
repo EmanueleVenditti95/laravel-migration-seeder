c48c831d4ba8a4f22d7677ed3bde8927272c4210
EmanueleVenditti95 <vendittiema@gmail.com>
2023-12-13 16:10:55 +0100

    Add - print all trains on screen in table

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,5 +4,34 @@
 <div class="container mt-5">
     <h1 class="mb-4">{{$title}} :</h1>
 
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Compagnia</th>
+                <th>Stazione Partenza</th>
+                <th>Stazione Arrivo</th>
+                <th>Orario Partenza</th>
+                <th>Orario Arrivo</th>
+                <th>Codice Treno</th>
+                <th>In orario</th>
+                <th>Cancellato</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                <tr>
+                    <td>{{$train->company}}</td>
+                    <td>{{$train->departure_station}}</td>
+                    <td>{{$train->arrival_station}}</td>
+                    <td>{{$train->departure_time}}</td>
+                    <td>{{$train->arrival_time}}</td>
+                    <td>{{$train->train_code}}</td>
+                    <td>{{$train->on_time}}</td>
+                    <td>{{$train->cancelled}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
 </div>
 @endsection
